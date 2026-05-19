1cd9dee2f24615671f96251f40e1bcc421754843
Juan Gabriel Borsacchi Marques <112633232+Juan-Borsacchi@users.noreply.github.com>
2026-05-19 18:23:53 -0300

    Add files via upload

---
+++ JAMPE!/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  JAMPE!
+//
+//  Created by Juan Gabriel Borsacchi Marques on 18/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ JAMPE!/JAMPE_App.swift
@@ -0,0 +1,17 @@
+//
+//  JAMPE_App.swift
+//  JAMPE!
+//
+//  Created by Juan Gabriel Borsacchi Marques on 18/05/26.
+//
+
+import SwiftUI
+
+@main
+struct JAMPE_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
