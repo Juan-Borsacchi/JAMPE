32e34b59ffc5738d8adbefe47dea1dfb8d973333
Pedro Teruel <pedrohteruel@gmail.com>
2026-05-21 14:23:56 -0300

    Criação da SplashScreen com elementos de imagem e texto.

---
+++ JAMPE!/SplashScreen.swift
@@ -0,0 +1,18 @@
+//
+//  SplashScreen.swift
+//  JAMPE!
+//
+//  Created by Pedro Henrique Hossaka Teruel on 20/05/26.
+//
+
+import SwiftUI
+
+struct SplashScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashScreen()
+}
