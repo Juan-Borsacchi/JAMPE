5391f27ec961aeb270c33c58e2e347189886e330
Agatha Santos <agathabsantos2@gmail.com>
2026-05-22 15:06:51 -0300

    Adicionado elementos na tela

--- JAMPE!/TelaSimulacao.swift
+++ JAMPE!/TelaSimulacao.swift
@@ -11,12 +11,22 @@ struct TelaSimulacao: View {
     var body: some View {
         VStack {
             Text("Resultado da Simulação")
-                .foregroundStyle(Color.azulTitulo)
+                .foregroundStyle(Color.restBlue)
                 .font(.system(size: 28, weight: .bold,design: .default))
             
             Text("Áreas afetadas na posição: Líbero")
             
+            Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ")
+            
             Text("Sugestão de horas de descanso")
+            
+            RecoveryTimeCard(hours: 72)
+            
+            PrimaryButton(title: "Nova simulação"){
+                print("..")
+            }
+            .padding(.horizontal)
+            
         }
     }
 }
