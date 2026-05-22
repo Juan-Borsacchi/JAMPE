a6f557912ce5ae12ea3a24ed36590e3102a70f23
Agatha Santos <agathabsantos2@gmail.com>
2026-05-22 14:46:29 -0300

    Tela de Simulacao Iniciada

--- JAMPE!/TelaSimulacao.swift
+++ JAMPE!/TelaSimulacao.swift
@@ -7,4 +7,20 @@
 
 import SwiftUI
 
+struct TelaSimulacao: View {
+    var body: some View {
+        VStack {
+            Text("Resultado da Simulação")
+                .foregroundStyle(Color.azulTitulo)
+                .font(.system(size: 28, weight: .bold,design: .default))
+            
+            Text("Áreas afetadas na posição: Líbero")
+            
+            Text("Sugestão de horas de descanso")
+        }
+    }
+}
 
+#Preview {
+    TelaSimulacao()
+}
